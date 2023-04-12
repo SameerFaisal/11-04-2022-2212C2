d34e6904056939c820eef14dd9d2bd4ba5d769b2
Sameer Faisal <45840879+SameerFaisal@users.noreply.github.com>
2023-04-12 12:05:41 +0500

    11-04-2023

---
+++ 2212C2 (11-04-2023)/index.js
@@ -0,0 +1,93 @@
+// let time=new Date()
+
+// let hours=time.getHours()
+// let minutes=time.getMinutes()
+// let seconds=time.getSeconds()
+// let state=""
+// if(hours<=12){
+//     state="AM"
+// }
+// else if(hours>12){
+//     state="PM"
+//     hours=hours-12
+// }
+// document.write(hours+" : "+minutes+" : "+seconds+" "+state)
+
+// function getDetails(){
+//     let n=document.getElementById("name").value
+//     let e=document.getElementById("email").value
+//     let p=document.getElementById("phone").value
+//     let m=document.getElementById("message").value
+//   let result="MR. "+n+" has email "+e+" has phone number "+p+" Wants to say "+m 
+// document.getElementById("output").innerHTML=result
+// }
+
+// let fn=document.getElementById("fn").value
+// let ln=document.getElementById("sn").value
+
+// function add(){
+//     result=parseFloat(fn)+parseFloat(ln)
+//     alert(result)
+
+// // document.getElementById("output").innerHTML=parseFloat(fn)+parseFloat(ln)
+// }
+// function sub(){
+// document.getElementById("output").innerHTML=parseFloat(fn)-parseFloat(ln)
+// }
+// function mul(){
+// document.getElementById("output").innerHTML=parseFloat(fn)*parseFloat(ln)
+// }
+// function div(){
+// document.getElementById("output").innerHTML=parseFloat(fn)/parseFloat(ln)
+// }
+
+// alert("Welcome to ABC Bank")
+// let balance=10000
+
+// function withdraw(){
+//     let amount=+prompt("Enter amount to withdraw")
+//     if(amount>balance){
+//         alert("Insufficient Balance")
+//     }
+//     else{
+//         alert("Balance before withdraw: "+balance)
+//         balance=balance-amount
+//         alert("Withdraw Successful")
+//     }
+// }
+// function deposit(){
+//     let amount=+prompt("Enter amount to deposit")
+//     alert("Balance before deposit: "+balance)
+//     balance=balance+amount
+//     alert("Deposit Successful")
+
+// }
+
+// function showBalance(){
+//     document.getElementById("output").innerHTML=balance
+// }
+
+// function change(){
+//    document.getElementById("img").src="./tomb.jfif"
+    
+// }
+// function reverse()
+// {
+//     document.getElementById("img").src="./istockphoto-184944186-612x612.jpg"
+// }
+
+// function doSomething(){
+//     document.getElementById("p1").classList.add("para")
+// }
+// function revert(){
+//     document.getElementById("p1").classList.remove("para")
+// }
+
+// const add=()=>{
+//     let n1=parseFloat(prompt("Enter number 1"))
+//     let n2=parseFloat(prompt("Enter number 2"))
+//     let result=n1+n2
+//     alert(result)
+// }
+
+// add()
